0856863af9c89ee83ca9c322b9af99eb02386e06
marcusLXZ <52640180+marcusLXZ@users.noreply.github.com>
2025-12-15 23:27:34 +0000

    Add a simple adk testing agent

---
+++ python/agents/academic-research/simple_agent.py
@@ -0,0 +1,69 @@
+import json
+from typing import Any
+
+from google.adk.agents import llm_agent
+from google.adk.sessions import in_memory_session_service
+import google.auth
+import vertexai
+from vertexai.preview.reasoning_engines import AdkApp
+
+
+InMemorySessionService = in_memory_session_service.InMemorySessionService
+
+
+class ProductNameGenerator:
+  """Generates product names using an ADK application."""
+
+  def __init__(self, project_id):
+
+    self.project_id = project_id
+    self.app = None
+
+  def session_service_builder(self):
+    """Builds the session service to use in the ADK app."""
+    return InMemorySessionService()
+
+  def set_up(self):
+    """Sets up the Vertex AI environment and the ADK application."""
+
+    creds, _ = google.auth.default(quota_project_id=self.project_id)
+
+    vertexai.init(
+        project=self.project_id,
+        location="europe-west3",
+        credentials=creds,
+    )
+
+    root_agent = llm_agent.LlmAgent(
+        name="ProductNameGeneratorAgent",
+        model="gemini-2.0-flash",
+        description="Agent to generate product names.",
+    )
+
+    self.app = AdkApp(
+        agent=root_agent,
+        enable_tracing=True,
+        session_service_builder=self.session_service_builder,
+    )
+
+  def stream_query(self, product: str) -> Any:
+    for chunk in self.app.stream_query(
+        message=f"What is a funny name for a company that makes {product}?",
+        user_id="test",
+    ):
+      yield chunk
+
+
+class FixedProductNameGenerator():
+
+  def stream_query(self, product: str) -> Any:
+    response_data = {
+        "output": "updated-reasoning-engine-prober: expected query response"
+    }
+    yield json.dumps(response_data)
+
+
+prod_name_generator = ProductNameGenerator(
+    project_id="cloud-aiplatform-prober"
+)
+fixed_name_generator = FixedProductNameGenerator()
